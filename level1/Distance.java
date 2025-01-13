d5dd65ee4c61da5fcb4046896d9d779945d44332
Vivek Omkar Machade <vivekmachade323@gmail.com>
2025-01-13 10:57:43 +0530

    Distance km to miles

---
+++ level1/Distance.java
@@ -0,0 +1,14 @@
+
+class Distance{
+	public static void main(String[] args){
+
+		// kilometers is 10.8
+		double kiloMeters = 10.8;
+
+		// 1 km = 0.6 miles
+		double mile = 0.6*kiloMeters;
+
+		System.out.println("The distance "+ kiloMeters +"km in miles is "+mile);
+
+	}
+}

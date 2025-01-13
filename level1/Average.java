20a1e82a90d1f80ccb7dbf7b4c9f6cd0c51d701b
Vivek Omkar Machade <vivekmachade323@gmail.com>
2025-01-13 10:45:26 +0530

    Find Average Program

---
+++ level1/Average.java
@@ -0,0 +1,18 @@
+class Average {
+    public static void main(String[] args){
+
+		// Sam mark in Maths is 94
+		int maths = 94;
+
+		// Sam mark in Physics is 95
+		int physics = 95;
+
+		// Sam mark in Chemistry is 96
+		int chemistry = 96;
+
+		//Average percent mark in PCM
+		double avg = (maths+physics+chemistry)/3;
+
+		System.out.println("Sam's average mark in PCM is " + avg);		
+	}
+}
